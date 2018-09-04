4a30eb80f1298570595ff8e523563a15c3ca920b
dmed256 <dmed256@gmail.com>
2018-09-03 23:45:58 -0500

    [C] Core classes can be base classes

--- occa/c/memory.cpp
+++ occa/c/memory.cpp
@@ -254,43 +254,43 @@ OCCA_PY_METHODS(
 
 static PyTypeObject MemoryType = {
   PyVarObject_HEAD_INIT(NULL, 0)
-  "occa.c.Memory",             // tp_name
-  sizeof(Memory),              // tp_basicsize
-  0,                           // tp_itemsize
-  (destructor) Memory_dealloc, // tp_dealloc
-  0,                           // tp_print
-  0,                           // tp_getattr
-  0,                           // tp_setattr
-  0,                           // tp_reserved
-  0,                           // tp_repr
-  0,                           // tp_as_number
-  0,                           // tp_as_sequence
-  0,                           // tp_as_mapping
-  0,                           // tp_hash
-  0,                           // tp_call
-  0,                           // tp_str
-  0,                           // tp_getattro
-  0,                           // tp_setattro
-  0,                           // tp_as_buffer
-  Py_TPFLAGS_DEFAULT,          // tp_flags
-  "Wrapper for occa::memory",  // tp_doc
-  0,                           // tp_traverse
-  0,                           // tp_clear
-  0,                           // tp_richcompare
-  0,                           // tp_weaklistoffset
-  0,                           // tp_iter
-  0,                           // tp_iternext
-  Memory_methods,              // tp_methods
-  0,                           // tp_members
-  0,                           // tp_getset
-  0,                           // tp_base
-  0,                           // tp_dict
-  0,                           // tp_descr_get
-  0,                           // tp_descr_set
-  0,                           // tp_dictoffset
-  (initproc) Memory_init,      // tp_init
-  0,                           // tp_alloc
-  0                            // tp_new
+  "occa.c.Memory",                          // tp_name
+  sizeof(Memory),                           // tp_basicsize
+  0,                                        // tp_itemsize
+  (destructor) Memory_dealloc,              // tp_dealloc
+  0,                                        // tp_print
+  0,                                        // tp_getattr
+  0,                                        // tp_setattr
+  0,                                        // tp_reserved
+  0,                                        // tp_repr
+  0,                                        // tp_as_number
+  0,                                        // tp_as_sequence
+  0,                                        // tp_as_mapping
+  0,                                        // tp_hash
+  0,                                        // tp_call
+  0,                                        // tp_str
+  0,                                        // tp_getattro
+  0,                                        // tp_setattro
+  0,                                        // tp_as_buffer
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, // tp_flags
+  "Wrapper for occa::memory",               // tp_doc
+  0,                                        // tp_traverse
+  0,                                        // tp_clear
+  0,                                        // tp_richcompare
+  0,                                        // tp_weaklistoffset
+  0,                                        // tp_iter
+  0,                                        // tp_iternext
+  Memory_methods,                           // tp_methods
+  0,                                        // tp_members
+  0,                                        // tp_getset
+  0,                                        // tp_base
+  0,                                        // tp_dict
+  0,                                        // tp_descr_get
+  0,                                        // tp_descr_set
+  0,                                        // tp_dictoffset
+  (initproc) Memory_init,                   // tp_init
+  0,                                        // tp_alloc
+  0                                         // tp_new
 };
 
 static bool memory_has_valid_module() {
